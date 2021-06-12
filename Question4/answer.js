51e79a17b30685e352f35f6be2a3644b364a692e
Adwi Arifin <adwiarifin@gmail.com>
2021-06-12 15:26:14 +0700

    Q4 - build indexing function

---
+++ Question4/answer.js
@@ -0,0 +1,35 @@
+const mapWord = (word) => {
+    // count occurence each character
+    const x = {};
+    for (let i = 0; i < word.length; i++) {
+        const c = word[i];
+        if (x[c]) {
+            x[c]++
+        } else {
+            x[c] = 1;
+        }
+    }
+
+    // build index
+    const sortedKeys = Object.keys(x).sort();
+    let result = '';
+    for (let i = 0; i < sortedKeys.length; i++) {
+        result += `${sortedKeys[i]}${x[sortedKeys[i]]}`;
+    }
+
+    return result;
+}
+
+const main = (arr) => {
+    const map = [];
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        const word = mapWord(element);
+        map[i] = word;
+    }
+    console.log(arr, map);
+};
+
+const input = ['kita', 'atik', 'tika', 'aku', 'kia', 'makan', 'kua'];
+const answer = main(input);
+// console.log(answer);
