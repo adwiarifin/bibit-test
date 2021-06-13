045f36fb7d122a2cccc78071c35dfd9b70006414
Adwi Arifin <adwiarifin@gmail.com>
2021-06-13 20:00:35 +0700

    q2 - cleaning code

--- Question2/app.js
+++ Question2/app.js
@@ -2,25 +2,12 @@ require('dotenv').config();
 require('express-group-routes');
 
 const express = require('express');
-const knex = require('knex');
 const routes = require('./routes');
 
 process.on('uncaughtException', (e) => {
     console.error(e);
     process.exit(0);
 });
-
-// DATABASE //
-const db = knex({
-    client: 'mysql',
-    connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-    }
-});
-// DATABASE //
 
 const app = express();
 
